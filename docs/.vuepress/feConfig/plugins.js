74e7484b5b785e808994586338438cd45d16a96d
renquanxi <renquanxi@aiyunxiao.com>
2021-05-06 15:18:36 +0800

    添加搜索和点心插件

--- docs/.vuepress/feConfig/plugins.js
+++ docs/.vuepress/feConfig/plugins.js
@@ -1,5 +1,11 @@
 module.exports = [
   [
+    require('../plugins/love-me'), {
+      color: '#11a8cd', // 爱心颜色，默认随机色
+    }
+  ],
+
+  [
     'vuepress-plugin-helper-live2d', {
       live2d: {
         // 是否启用(关闭请设置为false)(default: true)
@@ -22,5 +28,49 @@ module.exports = [
         }
       }
     }
-  ]
+  ],
+
+  // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+  [
+    'thirdparty-search',
+    {
+      thirdparty: [
+        // 可选，默认 []
+        // {
+        //   title: '在MDN中搜索',
+        //   frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+        //   behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+        // },
+        // {
+        //   title: '在Runoob中搜索',
+        //   frontUrl: 'https://www.runoob.com/?s=',
+        // },
+        {
+          title: '在Vue API中搜索',
+          frontUrl: 'https://cn.vuejs.org/v2/api/#',
+        },
+        {
+          title: '在Bing中搜索',
+          frontUrl: 'https://cn.bing.com/search?q=',
+        },
+        {
+          title: '通过百度搜索本站的',
+          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
+        },
+        {
+          title: '通过github搜索',
+          frontUrl: 'https://github.com/search?q=',
+        },
+        {
+          title: '通过google搜索',
+          frontUrl: 'https://www.google.com/search?q=',
+        },
+        {
+          title: '通过知乎搜索',
+          frontUrl: 'https://www.zhihu.com/search?q=',
+        },
+      ],
+    },
+  ],
+
 ]
